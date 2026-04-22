639fd4b42678af68091cc25e5da7316816e8f825
manav1913 <69932908+manav1913@users.noreply.github.com>
2026-04-23 03:32:22 +0500

    initial homepage setup

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import type { Post } from './PostList'
+
+interface Props{
+    post:Post
+}
+
+const PostItem = ({post}:Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostItem
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import PostList from '../components/PostList'
 const Home = () => {
   return (
     <div>
-      
+      <h2>Recent Posts</h2>
+      <div>
+        <PostList/>
+      </div>
     </div>
   )
 }
